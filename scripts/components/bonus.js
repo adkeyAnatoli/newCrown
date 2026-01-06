cad23b83b42547555eab99ebbe885d628c869177
zdan3v1ch <anatoli.zdanevitch@gmail.com>
2026-01-06 18:37:53 +0300

    fix: fix 1.6

--- scripts/components/bonus.js
+++ scripts/components/bonus.js
@@ -1,27 +1,21 @@
-import { qs, createEl } from '../utils/dom.js';
-import { navigateToOffer } from '../utils/navigation.js';
-import { appState } from '../state/app-state.js';
+import { qs, createEl } from "../utils/dom.js";
+import { navigateToOffer } from "../utils/navigation.js";
+import { appState } from "../state/app-state.js";
 
 export function renderBonus() {
   const mount = qs("#bonus");
   if (!mount) return;
-  
 
-  const desktop = qs(".bonus-text.desktop");
-  desktop.innerHTML = `Exclusive welcome offer of <span>${
-    appState.offer?.bonuses?.welcome_bonus || ""
-  }</span>`;
-  
-  const mobile = qs(".bonus-text.mobile");
-  mobile.innerHTML = `Exclusive welcome bonus of <span>${
-    appState.offer?.bonuses?.welcome_bonus || ""
-  }</span>`;
-  
-  const action = qs('.bonusButton');
+  const desktop = qs(".bonus-text.desktop span");
+  desktop.innerHTML = `${appState.offer?.bonuses?.welcome_bonus || ""}`;
+
+  const mobile = qs(".bonus-text.mobile span");
+  mobile.innerHTML = `${appState.offer?.bonuses?.welcome_bonus || ""}`;
+
+  const action = qs(".bonusButton");
   action.textContent = "Claim Bonus";
   action.addEventListener("click", (ev) => {
     ev.preventDefault();
-    if (appState.offer) navigateToOffer(appState.offer.id ,appState.offer.link);
+    if (appState.offer) navigateToOffer(appState.offer.id, appState.offer.link);
   });
-  
 }
